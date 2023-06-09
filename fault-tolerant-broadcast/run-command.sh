985ee244822c219f9208029ac454df47c10f1621
Nachiket Kanore <nachiket.kanore@gmail.com>
2023-06-09 23:43:21 +0530

    incorrect solution accepted for fault-tolerance challenge

--- fault-tolerant-broadcast/run-command.sh
+++ fault-tolerant-broadcast/run-command.sh
@@ -1,2 +1,3 @@
+# this solution fails - incorrect passed earlier
 cargo build --release
 ~/maelstrom/maelstrom/maelstrom test -w broadcast --bin ~/distributed-systems-challenges/fault-tolerant-broadcast/target/release/fault-tolerant-broadcast --node-count 5 --time-limit 20 --rate 10 --nemesis partition
